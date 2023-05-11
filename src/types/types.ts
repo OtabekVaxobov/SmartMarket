e38f662d49b74be6d98e6ce1929f40e2fa127705
Otabek_Vaxobov <elot2017@mail.ru>
2023-05-11 20:24:05 +0500

    fix

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,13 @@
-
 export interface Idata {
-  body: string
-  id: number
-  title: string
-  userId: number
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
 }
 export interface Itarget {
-
   target: { value: React.SetStateAction<string> };
-
 }
+
+export type BoxProps = {
+  children: React.ReactNode; // 👈️ type children
+};
