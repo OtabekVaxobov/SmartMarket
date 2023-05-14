45f1ca9712d44801d5aeeef7c2e5295362ea7621
Otabek_Vaxobov <elot2017@mail.ru>
2023-05-14 22:33:06 +0500

    feat: add layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import App from './App';
 import './style.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import Header from "./sections/Header";
-import Footer from "./sections/Footer";
 
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <Header />
     <App />
-    <Footer />
   </BrowserRouter>
 );
